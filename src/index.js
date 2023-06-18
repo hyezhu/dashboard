13033cebdf375eaa9ec4376bd8a7a5137adf36a8
hyezhu <huyennhu@me.com>
2023-06-19 00:42:33 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+// import { registerLicense } from "@syncfusion/ej2-base";
+
+// registerLicense(
+//   "Mgo+DSMBaFt+QHJqVk1hXk5Hd0BLVGpAblJ3T2ZQdVt5ZDU7a15RRnVfRFxgSXpTc0VhXH1edA==;Mgo+DSMBPh8sVXJ1S0R+X1pFdEBBXHxAd1p/VWJYdVt5flBPcDwsT3RfQF5jT39QdkFjXH9ceHBdQg==;ORg4AjUWIQA/Gnt2VFhiQlJPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9gSXhSdUVnWXpecnxcT2Q=;MjQwMDM2M0AzMjMxMmUzMDJlMzBiOENQc1JGd3pCNTUyQUI3OXhjZ2I2b0xzUnFIYll3aHVVNC9Hb25NUCtzPQ==;MjQwMDM2NEAzMjMxMmUzMDJlMzBkbDY4WVJCQjEyc1U0Uy9oV2VNRDI5ZURzcmhub2h3VVNNQWVPZUtrdnZBPQ==;NRAiBiAaIQQuGjN/V0d+Xk9HfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5Vd0ZjXX9bcHdVQGVY;MjQwMDM2NkAzMjMxMmUzMDJlMzBNZFB4RnFXRkFFN21Vc2s1bXFCV3BLM1pkU2lPeFl1eGtoanN6TXJPTEpvPQ==;MjQwMDM2N0AzMjMxMmUzMDJlMzBaRUo1UERrWW1QU3B1aDhYZEM4aWMxNDV0Mm9naHMydm5JelpvSldHSDA4PQ==;Mgo+DSMBMAY9C3t2VFhiQlJPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9gSXhSdUVnWXpec3ZVQGQ=;MjQwMDM2OUAzMjMxMmUzMDJlMzBEYnBNYVFkOEhTMTR1UDRqVXRkSWlsTWVyZGdnV2tnWWkzUVhVNzA1UmJZPQ==;MjQwMDM3MEAzMjMxMmUzMDJlMzBWSDgwc1FWcUFjbVZkS055b0ZCS21NQmZaOHB6NVhCMit1NmdvbmxyNkV3PQ==;MjQwMDM3MUAzMjMxMmUzMDJlMzBNZFB4RnFXRkFFN21Vc2s1bXFCV3BLM1pkU2lPeFl1eGtoanN6TXJPTEpvPQ=="
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
